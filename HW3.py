35f7a596dd13311be61df9f0c4e112be7643b69b
Komandanto <komandato666@gmail.com>
2022-10-08 22:03:14 +0300

    add HW 4 and seminar4

--- HW3.py
+++ HW3.py
@@ -117,27 +117,41 @@
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
 
-def Fibonacci(n):
-    if n in [1, 2]:
-        return 1
-    else:
-        return Fibonacci(n-1) + Fibonacci(n-2)
+# def Fibonacci(n):
+#     if n in [1, 2]:
+#         return 1
+#     else:
+#         return Fibonacci(n-1) + Fibonacci(n-2)
+#
+# def NegaFibonacci(n):
+#     if n == 1:
+#         return 1
+#     elif n == 2:
+#         return -1
+#     else:
+#         num1, num2 = 1, -1
+#         for i in range(2, n):
+#             num1, num2 = num2, num1 - num2
+#         return num2
+#
+# my_list = [0]
+# k = int(input('Введите число: '))
+# for i in range(1, k + 1):
+#     my_list.append(Fibonacci(i))
+#     my_list.insert(0, NegaFibonacci(i))
+# print(my_list)
 
-def NegaFibonacci(n):
-    if n == 1:
-        return 1
-    elif n == 2:
-        return -1
-    else:
-        num1, num2 = 1, -1
-        for i in range(2, n):
-            num1, num2 = num2, num1 - num2
-        return num2
 
-my_list = [0]
-k = int(input('Введите число: '))
-for i in range(1, k + 1):
-    my_list.append(Fibonacci(i))
-    my_list.insert(0, NegaFibonacci(i))
-print(my_list)
-
+# n = 10
+# fib = [0, 1]
+#
+# for i in range(n - 1):
+#     fib.append(fib[-2] - fib[-1])
+#
+#
+# fib.reverse()
+#
+# for i in range(n):
+#     fib.append(fib[-1] + fib[-2])
+#
+# print(fib)
