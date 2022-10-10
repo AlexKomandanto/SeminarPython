55385c62ded56cf0ce0493a41698093eda166ca2
Komandanto <komandato666@gmail.com>
2022-10-10 23:17:18 +0300

    add HW 4 and seminar4

---
+++ seminar4.py
@@ -0,0 +1,20 @@
+#  Менее затратный для памяти, метод выполнения операции, списки
+# lst = []
+#
+# for i in range(22):
+#     lst.append(str(i))
+# lst.append('+')
+# s = ', '.join(lst)
+# print(s)
+#
+
+# mem = {1: 1, 2: 1}        # Менее затратный для памяти, метод выполнения операции
+#
+#
+# def fib(n):
+# if n not in mem:
+# mem[n] = fib(n - 1) + fib(n - 2)
+# return mem[n]
+#
+#
+# print(fib(405))
