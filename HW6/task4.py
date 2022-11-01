2433cd2c830fa521fb409e1433c8ecd3ffa61626
Komandanto <komandato666@gmail.com>
2022-11-01 18:45:47 +0300

    add  HW7

--- HW6/task4.py
+++ HW6/task4.py
@@ -16,4 +16,5 @@
 # Новое решение
 
 my_list = [1, 1, 2, 3, 3, 4, 1, 5, 7, 8, 8, 7, 9]
-print(f'Исходный список: {my_list}\nСписок неповторяющихся элементов: {list(filter(lambda i: my_list.count(i) ==1, my_list))}')
+print(f'Исходный список: {my_list}\nСписок неповторяющихся элементов:'f' {list(filter(lambda i: my_list.count(i) ==1, my_list))}')
+
